1acbecf606fb6d95408c3867640cbeebc3b4eb3c
mayowDev <dinnod4@gmail.com>
2019-12-16 04:48:27 +0500

    Header Section Compleat

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "../css/Header.css";
+import { BrowserRouter as Router, Link, Switch } from "react-router-dom";
+
+function Header() {
+  return (
+    <Router>
+      <header>
+        <nav>
+          <h2 className='logo'>
+            <Link to='/'>Foody..</Link>
+          </h2>
+          <ul className='nav-menu'>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/menu'>Menu</Link>
+            </li>
+            <li>
+              <Link to='/locations'>locations</Link>
+            </li>
+            <li>
+              <Link to='/search'>
+                <i className='fa fa-search'></i>
+              </Link>
+            </li>
+            <li>
+              <Link to='/booking'>
+                <i className='fa fa-shopping-bag'></i>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <h2 className='header-title'>Welcome to</h2>
+        <h1 className='header-description'>Monal Restaurant</h1>
+        <Link className='header-cta-btn' to='#'>
+          Book A Table
+        </Link>
+      </header>
+    </Router>
+  );
+}
+
+export default Header;
