71094fd5d825deb7d2381ff7c635aea8a95a9354
Dinno-dev <mayowdev@gmail.com>
2020-08-02 12:11:43 -0600

    refactor for build

---
+++ precache-manifest.158a148d9f69cba3d3e970d84cdfa4b6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb74c19c0971a0eae5d963e1472f31ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3f31c7577981ca877c1",
+    "url": "/static/css/main.fb24484d.chunk.css"
+  },
+  {
+    "revision": "1a1ec5fddc0f465e0c41",
+    "url": "/static/js/2.35850484.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.35850484.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e3f31c7577981ca877c1",
+    "url": "/static/js/main.01296794.chunk.js"
+  },
+  {
+    "revision": "f406634a162ba916b972",
+    "url": "/static/js/runtime-main.43d3cfe1.js"
+  },
+  {
+    "revision": "a50891ea34afa63ef8da185748e2a86c",
+    "url": "/static/media/breakfast.a50891ea.jpg"
+  },
+  {
+    "revision": "4909b7c99c3815b4f7ee9204ff39ccc9",
+    "url": "/static/media/dinner.4909b7c9.jpg"
+  },
+  {
+    "revision": "7123bd950a45ade159c25f32d22f3b10",
+    "url": "/static/media/footer.7123bd95.jpg"
+  },
+  {
+    "revision": "c54887379a79b417baa7d8b10cdb3459",
+    "url": "/static/media/hero1.c5488737.jpg"
+  },
+  {
+    "revision": "3e857308674bb8b2f7571347f9ff9c29",
+    "url": "/static/media/hyderabadi-biryani.3e857308.jpg"
+  },
+  {
+    "revision": "c7bd835373c5582f38aca12ae2879aa0",
+    "url": "/static/media/ice-cream.c7bd8353.jpg"
+  },
+  {
+    "revision": "36633c0e0667eb69db33813b03cfbbef",
+    "url": "/static/media/islamabad1.36633c0e.jpg"
+  },
+  {
+    "revision": "f37200354186204a607e0d7c76fe8996",
+    "url": "/static/media/lahore1.f3720035.jpg"
+  },
+  {
+    "revision": "517790e93405752915c530290a176d92",
+    "url": "/static/media/lunch.517790e9.jpg"
+  },
+  {
+    "revision": "a09273c47cd0e478ff70278bfd6f759e",
+    "url": "/static/media/menu.a09273c4.jpg"
+  },
+  {
+    "revision": "0aada563e7fc862f67d4aa0787fd96ac",
+    "url": "/static/media/piza.0aada563.jpg"
+  },
+  {
+    "revision": "548f59b4f0958d4ee0f3e3cbee415dc9",
+    "url": "/static/media/pizza.548f59b4.jpg"
+  },
+  {
+    "revision": "055332b3ef8ce5d654131cb60b86fdad",
+    "url": "/static/media/salmon-fish.055332b3.jpg"
+  }
+]);
